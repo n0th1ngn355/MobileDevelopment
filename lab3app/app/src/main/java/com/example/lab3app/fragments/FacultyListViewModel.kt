cc647af4b77beaf6514bd64ebfb82989b97dce9e
n0th1ngn355 <tigra2820@mail.ru>
2024-03-19 13:08:13 +0300

    temp3

--- lab3app/app/src/main/java/com/example/lab3app/fragments/FacultyListViewModel.kt
+++ lab3app/app/src/main/java/com/example/lab3app/fragments/FacultyListViewModel.kt
@@ -1,7 +1,53 @@
 package com.example.lab3app.fragments
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import com.example.lab3app.data.Faculty
+import com.example.lab3app.data.FacultyList
+import com.example.lab3app.repository.UniversityRepository
 
 class FacultyListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var facultyList: MutableLiveData<FacultyList> = MutableLiveData()
+
+    private var _faculty: Faculty? = null
+
+    val faculty
+        get() = _faculty
+
+    private val facultyListObserver = Observer<FacultyList?>{
+            list ->
+        facultyList.postValue(list!!)
+    }
+
+    init{
+        UniversityRepository.getInstance().facultyList.observeForever(facultyListObserver)
+        UniversityRepository.getInstance().faculty.observeForever{
+            _faculty = it
+        }
+    }
+
+    fun deleteFaculty(){
+        if(faculty != null){
+            UniversityRepository.getInstance().deleteFaculty(faculty!!)
+        }
+    }
+
+    fun appendFaculty(name: String, city: String){
+        val faculty = Faculty()
+        faculty.universityID = UniversityRepository.getInstance().university.value?.id
+        faculty.name = name
+        UniversityRepository.getInstance().newFaculty(faculty)
+    }
+
+    fun updateFaculty(name: String, city: String){
+        if (_faculty != null){
+            _faculty!!.name = name
+            UniversityRepository.getInstance().updateFaculty(_faculty!!)
+        }
+    }
+
+    fun setCurrentFaculty(faculty: Faculty){
+        UniversityRepository.getInstance().setCurrentFaculty(faculty)
+    }
 }
